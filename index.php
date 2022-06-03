db8b6aac26924521fef6c1310f3ef5e13fb5603e
YayaIniee <yayainiee@gmail.com>
2022-06-03 18:51:36 +0700

    memperbaharui css dan js

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>|[ Top Up Games Store ]|</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,37 @@
+<section id="layanan">
+  <div class="container">
+    <div class="row my-4">
+      <div class="col-12 text-center">
+        <h2>Layanan Kami</h2>
+        <span class="sub-title">pilih game kesukaan kamu dan top up segera!</span>
+      </div>
+    </div>
+    <div class="row">
+    <?php
+        require_once('./class/class.Game.php'); 
+			$objGame = new Game(); 				
+        	$arrayResult = $objGame->SelectAllGame(0, '');
+
+				foreach ($arrayResult as $dataGame) {
+
+                    echo '
+                    <div class="col-md-3 mb-4">
+                        <div class="card p-2" width="22rem">
+                            <a href="index.php?p=sectiongame&id='.$dataGame->id.'">
+                                <img src="./assets/upload/game/'.$dataGame->foto.'" alt="" class="img-fluid">
+                            </a>
+                            <div class="card-body">
+                                <h5><a href="index.php?p=sectiongame&id='.$dataGame->id.'" class="title">'.$dataGame->nama.'</a></h5>
+                            </div>
+                        </div>
+                    </div>
+                    ';
+                }
+                ?>      
+    </div>
+  </div>
+</section>
+
 <div class="container-fluid">
     <div class="container">
         <div class="row">
@@ -13,16 +47,17 @@
 
 					foreach ($arrayResult as $dataGame) {
 						echo '<div class="col-lg-3 col-md-4 col-sm-6 col-12 mt-3" >
-                			<a href="index.php?p=sectiongame&id='.$dataGame->id.'"><img src="./assets/upload/game/'.$dataGame->foto.'" alt="" class="img-fluid rounded-3" id=""></a>
-                			<a href="index.php?p=sectiongame&id='.$dataGame->id.'" class="title">'.$dataGame->nama.'</a>
-            			</div>
-							  
+                			  <a href="index.php?p=sectiongame&id='.$dataGame->id.'"><img src="./assets/upload/game/'.$dataGame->foto.'" alt="" class="img-fluid rounded-3" id=""></a>
+                			  <a href="index.php?p=sectiongame&id='.$dataGame->id.'" class="title">'.$dataGame->nama.'</a>
+            			      </div>
 							  ';
 					}
 					?>
             <div class="col-lg-2 col-md-4 col-sm-6 col-12 mt-3" >
-                <a href=""><img src="" alt="" class="img-fluid rounded-3" id=""></a>
+                <a href=""><img src="#" alt="" class="img-fluid rounded-3" id=""></a>
             </div>
         </div>
     </div>
 </div>
+
+
